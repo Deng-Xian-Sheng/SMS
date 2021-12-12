aa6c974f7b2d6ce7d596cc478fde6fa8b9c3f229
Deng-Xian-Sheng <x17615848429@sina.com>
2021-12-13 00:11:59 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,31 @@
+# SMS-学生管理系统
+
+# 注意⚠️！它还在开发中，现在什么也做不了
+
+
+
+# SMSUI
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ smsui/src/views/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-container>
+      <el-header><Header /></el-header>
+      <el-main><Main /></el-main>
+      <el-footer><Footer /></el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Header from "./header.vue";
+import Main from "./main.vue";
+import Footer from "./footer.vue";
+export default {
+  name: "Home",
+  components: {
+    Main,
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style>
+.el-header,
+.el-footer {
+  /* background-color: #d2e8ff; */
+  /* color: #303133; */
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  /* background-color: #d2e8ff; */
+  /* color: #303133; */
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  /* background-color: #e9f4ff; */
+  /* color: #303133; */
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
