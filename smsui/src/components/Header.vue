b835c6b45d7920649968fb83fdd2bd0c0444534b
Deng-Xian-Sheng <x17615848429@sina.com>
2021-12-22 19:26:00 +0800

    每日提交

--- smsui/src/components/Header.vue
+++ smsui/src/components/Header.vue
@@ -26,7 +26,7 @@
             <span>
               <el-col :span="12" :push="9" class="hidden-md-and-down">
                 <div class="grid-content bg-purple-light">
-                  <span v-on:click="TextLink" style="cursor:pointer;" >学生管理系统</span>
+                  <span v-on:click="TextLink" style="cursor:pointer;">学生管理系统</span>
                 </div>
               </el-col>
             </span>
@@ -51,9 +51,127 @@
         </div>
       </el-col>
       <el-col :span="6">
-        <div class="grid-content bg-purple">demo</div>
+        <div class="grid-content bg-purple">
+          <el-button type="text" @click="centerDialogVisible = true">注册/登录</el-button>
+        </div>
       </el-col>
     </el-row>
+    <el-dialog
+      title="登录"
+      :visible.sync="centerDialogVisible"
+      width="40%"
+      center
+      :destroy-on-close="true"
+      custom-class="RegisterLogin"
+    >
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">用户名：</div>
+        </el-col>
+        <el-col :span="20">
+          <div class="grid-content bg-purple-light">
+            <el-input placeholder="请输入用户名" v-model="inputName" clearable></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">密码：</div>
+        </el-col>
+        <el-col :span="20">
+          <div class="grid-content bg-purple-light">
+            <el-input placeholder="请输入密码" v-model="inputPasswd" show-password></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="4">
+          <div class="grid-content bg-purple">
+            <slide-verify
+              ref="slideblock"
+              @again="onAgain"
+              @fulfilled="onFulfilled"
+              @success="onSuccess"
+              @fail="onFail"
+              @refresh="onRefresh"
+              :slider-text="verificationCodeTips"
+              :accuracy="accuracy"
+              :imgs="slideblockImgs"
+            ></slide-verify>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="40">
+        <el-col :span="12" :offset="9">
+          <div class="grid-content bg-purple">
+            <el-button type="text" @click="centerDialogVisible = false;RegisterDialogVisible = true" >注册</el-button>
+            <span>/</span>
+            <el-button type="text">忘记密码</el-button>
+          </div>
+        </el-col>
+      </el-row>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary">马上登录</el-button>
+      </span>
+    </el-dialog>
+    <el-dialog
+      title="注册"
+      :visible.sync="RegisterDialogVisible"
+      width="40%"
+      center
+      :destroy-on-close="true"
+      custom-class="RegisterLogin"
+    >
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">用户名：</div>
+        </el-col>
+        <el-col :span="20">
+          <div class="grid-content bg-purple-light">
+            <el-input placeholder="请输入用户名" v-model="inputNameRegister" clearable></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">密码：</div>
+        </el-col>
+        <el-col :span="20">
+          <div class="grid-content bg-purple-light">
+            <el-input placeholder="请输入密码" v-model="inputPasswdRegister" show-password></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="4">
+          <div class="grid-content bg-purple">
+            <slide-verify
+              ref="slideblock"
+              @again="onAgain"
+              @fulfilled="onFulfilled"
+              @success="onSuccess"
+              @fail="onFail"
+              @refresh="onRefresh"
+              :slider-text="verificationCodeTips"
+              :accuracy="accuracy"
+              :imgs="slideblockImgs"
+            ></slide-verify>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="40">
+        <el-col :span="12" :offset="9">
+          <div class="grid-content bg-purple">
+            <el-button type="text" @click="RegisterDialogVisible = false;centerDialogVisible = true">登录</el-button>
+            <span>/</span>
+            <el-button type="text">忘记密码</el-button>
+          </div>
+        </el-col>
+      </el-row>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary">马上注册</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -65,10 +183,52 @@ export default {
       activeIndex: "1",
       fit: "cover",
       url: "/favicon.png",
+      centerDialogVisible: false,
+      inputName: "",
+      inputPasswd: "",
+      verificationCodeStatus: "",
+      verificationCodeTips: "向右滑动->",
+      // 精确度小，可允许的误差范围小；为1时，则表示滑块要与凹槽完全重叠，才能验证成功。默认值为5
+      accuracy: 1,
+      slideblockImgs: [
+        "/image/VerificationCodeImage/1023-310x155.jpg",
+        "/image/VerificationCodeImage/32-310x155.jpg",
+        "/image/VerificationCodeImage/433-310x155.jpg",
+        "/image/VerificationCodeImage/622-310x155.jpg",
+        "/image/VerificationCodeImage/87-310x155.jpg",
+        "/image/VerificationCodeImage/1072-310x155.jpg",
+        "/image/VerificationCodeImage/334-310x155.jpg",
+        "/image/VerificationCodeImage/503-310x155.jpg",
+        "/image/VerificationCodeImage/691-310x155.jpg",
+        "/image/VerificationCodeImage/931-310x155.jpg",
+        "/image/VerificationCodeImage/13-310x155.jpg",
+        "/image/VerificationCodeImage/342-310x155.jpg",
+        "/image/VerificationCodeImage/538-310x155.jpg",
+        "/image/VerificationCodeImage/701-310x155.jpg",
+        "/image/VerificationCodeImage/937-310x155.jpg",
+        "/image/VerificationCodeImage/145-310x155.jpg",
+        "/image/VerificationCodeImage/392-1-310x155.jpg",
+        "/image/VerificationCodeImage/554-310x155.jpg",
+        "/image/VerificationCodeImage/76-310x155.jpg",
+        "/image/VerificationCodeImage/943-310x155.jpg",
+        "/image/VerificationCodeImage/243-310x155.jpg",
+        "/image/VerificationCodeImage/392-310x155.jpg",
+        "/image/VerificationCodeImage/559-310x155.jpg",
+        "/image/VerificationCodeImage/820-310x155.jpg",
+        "/image/VerificationCodeImage/954-310x155.jpg",
+        "/image/VerificationCodeImage/272-310x155.jpg",
+        "/image/VerificationCodeImage/402-310x155.jpg",
+        "/image/VerificationCodeImage/575-310x155.jpg",
+        "/image/VerificationCodeImage/832-310x155.jpg",
+        "/image/VerificationCodeImage/988-310x155.jpg",
+      ],
+      RegisterDialogVisible: false,
+      inputNameRegister: "",
+      inputPasswdRegister: "",
     };
   },
   methods: {
-    handleSelect(key, keyPath) {
+    handleSelect(key) {
       if (key == 1 && this.$route.path != "/") {
         this.$router.push({ path: "/" });
       } else if (key == 2 && this.$route.path != "/studentmanagement") {
@@ -78,11 +238,41 @@ export default {
       } else if (key == 4 && this.$route.path != "/help") {
         this.$router.push({ path: "/help" });
       }
-      console.log(key, keyPath);
     },
-    TextLink:function(){
-      window.open("/")
+    TextLink: function () {
+      window.open("/");
+    },
+    onSuccess(times) {
+      console.log("验证通过，耗时" + times + "毫秒");
+      this.verificationCodeStatus =
+        "login success, 耗时${(times / 1000).toFixed(1)}s";
+    },
+    onFail() {
+      console.log("验证不通过");
+      this.verificationCodeStatus = "";
+    },
+    onRefresh() {
+      console.log("点击了刷新小图标");
+      this.verificationCodeStatus = "";
+    },
+    onFulfilled() {
+      console.log("刷新成功啦！");
+    },
+    onAgain() {
+      console.log("检测到非人为操作的哦！");
+      this.verificationCodeStatus = "try again";
+      // 刷新
+      this.$refs.slideblock.reset();
+    },
+    RefreshVerificationCode() {
+      this.$refs.slideblock.reset();
     },
   },
 };
 </script>
+<style>
+.RegisterLogin {
+  background-image: url("/image/RegisterLoginBackground.png") !important;
+  background-size: cover !important;
+}
+</style>
--- smsui/src/main.js
+++ smsui/src/main.js
@@ -4,10 +4,14 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/theme-chalk/display.css';
 import router from './router'
+import SlideVerify from 'vue-monoplasty-slide-verify';
+import axios from 'axios';
 
+Vue.prototype.axios = axios;
 Vue.config.productionTip = true
 
 Vue.use(ElementUI);
+Vue.use(SlideVerify);
 
 new Vue({
   el: '#app',
