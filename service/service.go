34dc2ee965ba4cc0f9dce87c5eece551000dfa2c
Deng-Xian-Sheng <x17615848429@sina.com>
2022-01-06 17:16:54 +0800

    补充提交

--- service/service.go
+++ service/service.go
@@ -6,33 +6,33 @@ import (
 	"log"
 	// "net/http"
 	"fmt"
-	"sms/database"
+	// "sms/database"
 	// "sms/controller"
-	"github.com/google/uuid"
-	"sms/authentication"
-	"time"
+	// "github.com/google/uuid"
+	// "sms/authentication"
+	// "time"
 )
 
 func Service(ServiceInfo, DatabaseInfo map[string]string) {
-	//准备好Jwt需要的key
-	JwtkeyString, err := database.CRUDAuthentication(DatabaseInfo, map[string]string{}, "RJwtkey")
-	if err != nil {
-		fmt.Println(err)
-	}
-	if JwtkeyString["RowsAffected"] == "0" {
-		NewJwtkey, err := uuid.NewRandom()
-		if err != nil {
-			log.Printf(fmt.Sprint(err))
-		}
-		_, err = database.CRUDAuthentication(DatabaseInfo, map[string]string{"NewJwtkey": fmt.Sprint(NewJwtkey)}, "UJwtkey")
-		if err != nil {
-			log.Printf(fmt.Sprint(err))
-		}
-		JwtkeyString, err = database.CRUDAuthentication(DatabaseInfo, map[string]string{}, "RJwtkey")
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+	// //准备好Jwt需要的key
+	// JwtkeyString, err := database.CRUDAuthentication(DatabaseInfo, map[string]string{}, "RJwtkey")
+	// if err != nil {
+	// 	log.Printf(fmt.Sprint(err))
+	// }
+	// if JwtkeyString["RowsAffected"] == "0" {
+	// 	NewJwtkey, err := uuid.NewRandom()
+	// 	if err != nil {
+	// 		log.Printf(fmt.Sprint(err))
+	// 	}
+	// 	_, err = database.CRUDAuthentication(DatabaseInfo, map[string]string{"NewJwtkey": fmt.Sprint(NewJwtkey)}, "UJwtkey")
+	// 	if err != nil {
+	// 		log.Printf(fmt.Sprint(err))
+	// 	}
+	// 	JwtkeyString, err = database.CRUDAuthentication(DatabaseInfo, map[string]string{}, "RJwtkey")
+	// 	if err != nil {
+	// 		log.Printf(fmt.Sprint(err))
+	// 	}
+	// }
 
 	router := gin.Default()
 	router.POST("/Register", func(c *gin.Context) {
@@ -48,21 +48,21 @@ func Service(ServiceInfo, DatabaseInfo map[string]string) {
 	})
 	router.Run(":" + ServiceInfo["Port"])
 
-	//生成Token
-	TokenString, err := authentication.Setting(map[string]string{"ID": "39043D95-DDF5-40A7-B8B2-53229F6C6E41", "JwtkeyString": JwtkeyString["Jwtkey"]}, time.Now().Add(24*time.Hour))
-	if err != nil {
-		log.Printf(fmt.Sprint(err))
-	}
+	// //生成Token
+	// TokenString, err := authentication.Setting(map[string]string{"ID": "39043D95-DDF5-40A7-B8B2-53229F6C6E41", "JwtkeyString": JwtkeyString["Jwtkey"]}, time.Now().Add(24*time.Hour))
+	// if err != nil {
+	// 	log.Printf(fmt.Sprint(err))
+	// }
 
-	//验证Token
-	ID, err := authentication.Getting(map[string]string{"TokenString": TokenString, "JwtkeyString": JwtkeyString["Jwtkey"]})
-	if err != nil {
-		log.Printf(fmt.Sprint(err))
-	}
+	// //验证Token
+	// ID, err := authentication.Getting(map[string]string{"TokenString": TokenString, "JwtkeyString": JwtkeyString["Jwtkey"]})
+	// if err != nil {
+	// 	log.Printf(fmt.Sprint(err))
+	// }
 
-	if ID == "" {
-		fmt.Println("验证失败")
-	} else {
-		fmt.Println(ID)
-	}
+	// if ID == "" {
+	// 	fmt.Println("验证失败")
+	// } else {
+	// 	fmt.Println(ID)
+	// }
 }
