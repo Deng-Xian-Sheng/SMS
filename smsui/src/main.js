ff1e3257d575ed6ceb796808c89f01840384deba
Deng-Xian-Sheng <x17615848429@sina.com>
2022-01-06 17:13:01 +0800

    smsui处理title，处理404页

--- smsui/src/main.js
+++ smsui/src/main.js
@@ -4,17 +4,17 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/theme-chalk/display.css';
 import router from './router'
-import SlideVerify from 'vue-monoplasty-slide-verify';
 import axios from 'axios';
+import VueWechatTitle from 'vue-wechat-title'
 
 Vue.prototype.axios = axios;
 Vue.config.productionTip = true;
 
 Vue.use(ElementUI);
-Vue.use(SlideVerify);
+Vue.use(VueWechatTitle)
 
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 });
--- smsui/src/router/index.js
+++ smsui/src/router/index.js
@@ -4,46 +4,81 @@ import Home from '../views/Home.vue'
 import StudentManagement from '../views/StudentManagement.vue'
 import AnnouncementCenter from '../views/AnnouncementCenter.vue'
 import Help from '../views/Help.vue'
+import NotFoundComponent from '@/views/NotFoundComponent'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    components: {
-      boss: Home,
+const routes = [{
+        path: '/',
+        name: 'Home',
+        components: {
+            boss: Home,
+        },
+        meta: {
+            // 页面标题title
+            title: '首页',
+        },
     },
-  },
-  {
-    path: '/studentmanagement',
-    name: 'StudentManagement',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    components: {
-      boss: StudentManagement,
+    {
+        path: '/studentmanagement',
+        name: 'StudentManagement',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        components: {
+            boss: StudentManagement,
+        },
+        meta: {
+            // 页面标题title
+            title: '学生管理',
+        },
     },
-  },
-  {
-    path: '/announcementcenter',
-    name: 'AnnouncementCenter',
-    components:{
-      boss:AnnouncementCenter,
+    {
+        path: '/announcementcenter',
+        name: 'AnnouncementCenter',
+        components: {
+            boss: AnnouncementCenter,
+        },
+        meta: {
+            // 页面标题title
+            title: '公告中心',
+        },
     },
-  },
-  {
-    path:'/help',
-    name:'Help',
-    components:{
-      boss:Help,
+    {
+        path: '/help',
+        name: 'Help',
+        components: {
+            boss: Help,
+        },
+        meta: {
+            // 页面标题title
+            title: '帮助说明',
+        },
     },
-  },
+    {
+        path: '*',
+        name: 'NotFoundComponent',
+        components: {
+            boss: NotFoundComponent,
+        },
+        meta: {
+            // 页面标题title
+            title: '404',
+        },
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    mode: 'history',
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    /* 路由发生变化修改页面title */
+    if (to.meta.title) {
+        document.title = to.meta.title
+    }
+    next()
 })
 
 export default router
